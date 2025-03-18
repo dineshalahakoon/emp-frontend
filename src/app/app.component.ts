be82cb626d7df30621d7684a39ccec5a71529a16
dineshalahakoon <dineshroxx1997@gmail.com>
2025-03-18 14:28:46 +0530

    view all employee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AddEmployeeComponent } from './page/add-employee/add-employee.component';
 import { ManageEmployeeComponent } from './page/manage-employee/manage-employee.component';
+import { ViewAllEmployeeComponent } from './page/view-all-employee/view-all-employee.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ManageEmployeeComponent],
+  imports: [RouterOutlet,ManageEmployeeComponent,ViewAllEmployeeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
